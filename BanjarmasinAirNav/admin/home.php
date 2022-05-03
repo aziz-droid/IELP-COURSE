d2030eea96fa98d1513c80ff51a423ea02299017
abdul aziz <Abdul.19087@mhs.unesa.ac.id>
2022-05-04 00:47:08 +0700

    update kurang

--- BanjarmasinAirNav/admin/home.php
+++ BanjarmasinAirNav/admin/home.php
@@ -11,7 +11,7 @@
   <div class="main-content">
         <section class="section">
           <div class="section-header">
-            <h1>IELP TRAINING FOR POLTEKBANG SURABAYA</h1>
+            <h1>IELP TRAINING FOR POLTEKBANG SIDOARJO</h1>
             <div class="section-header-breadcrumb">
               <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
               <!-- <div class="breadcrumb-item">Profile</div> -->
