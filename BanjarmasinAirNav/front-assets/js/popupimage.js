62ac1e611e36d07741dc2fa060df073e8ea612d8
mas-nano <nabilislam8@gmail.com>
2022-05-16 13:53:45 +0700

    init laravel app

--- BanjarmasinAirNav/front-assets/js/popupimage.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $(".img").click(function () {
-        var img = $(this).attr('src');
-        $("#show-img").attr('src', img);
-        $("#imgmodal").modal('show');
-    });
-});
